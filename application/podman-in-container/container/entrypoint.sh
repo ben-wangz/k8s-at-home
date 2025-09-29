25ed1f119c03f2c95de9b40d0f8a855ccd5fb5d9
ben.wangz <ben.wangz@foxmail.com>
2025-09-29 10:06:08 +0000

    fix(entrypoint): ensure SSH host keys exist before starting SSH service

--- application/podman-in-container/container/entrypoint.sh
+++ application/podman-in-container/container/entrypoint.sh
@@ -4,6 +4,11 @@ set -o errexit -o nounset -o pipefail
 
 ENABLE_SSH=${ENABLE_SSH:-"true"}
 if [[ "${ENABLE_SSH}" == "true" ]]; then
+  if [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
+    echo "Generating SSH host keys..."
+    ssh-keygen -A
+  fi
+  
   if [[ -n "${AUTHORIZED_KEYS:-}" ]]; then
     mkdir -p $HOME/.ssh
     chmod 700 $HOME/.ssh
