9e484ea7a12c9c17c0c733a7c7b8d138979b812b
ben.wangz <ben.wangz@foxmail.com>
2025-10-10 09:33:30 +0000

    chore: bump version to 1.1.1 and fix ssh directory permissions
    Ensure SSH directory permissions are set before handling authorized keys

--- application/podman-in-container/container/entrypoint.sh
+++ application/podman-in-container/container/entrypoint.sh
@@ -4,14 +4,13 @@ set -o errexit -o nounset -o pipefail
 
 ENABLE_SSH=${ENABLE_SSH:-"true"}
 if [[ "${ENABLE_SSH}" == "true" ]]; then
+  mkdir -p $HOME/.ssh
+  chmod 700 $HOME $HOME/.ssh
   if [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
     echo "Generating SSH host keys..."
     ssh-keygen -A
   fi
-  
   if [[ -n "${AUTHORIZED_KEYS:-}" ]]; then
-    mkdir -p $HOME/.ssh
-    chmod 700 $HOME/.ssh
     echo "${AUTHORIZED_KEYS}" > $HOME/.ssh/authorized_keys
     chmod 600 $HOME/.ssh/authorized_keys
     echo "SSH authorized keys have been configured by env: AUTHORIZED_KEYS"
